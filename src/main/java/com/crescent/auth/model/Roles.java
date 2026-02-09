38448127bd1017d409e263280e4fd2ff20870bd5
Qamar Islam <qamarislam.java@gmail.com>
2026-02-09 23:21:18 +0300

    tested by newly added service doctor,patient,appointment

--- src/main/java/com/crescent/auth/model/Roles.java
+++ src/main/java/com/crescent/auth/model/Roles.java
@@ -4,5 +4,8 @@ public enum Roles {
     ROLE_ADMIN,
     ROLE_DOCTOR,
     ROLE_PATIENT,
-    ROLE_EMPLOYEE
+    ROLE_EMPLOYEE,
+    ROLE_RECEPTIONIST, // New
+    ROLE_NURSE,        // New
+    ROLE_PHARMACIST    // New
 }
